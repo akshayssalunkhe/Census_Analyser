827cb8279bb4f3c3cf173bc13e5d080a91c7750a
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-08-14 20:32:26 +0530

    [Akshay]ADDED:Test Case To Check Wrong File Path

--- CensusAnalyserTest/CensusAnalyserTestCases.cs
+++ CensusAnalyserTest/CensusAnalyserTestCases.cs
@@ -1,3 +1,4 @@
+using CensusAnalyser;
 using NUnit.Framework;
 
 namespace CensusAnalyserTest
@@ -5,6 +6,8 @@ namespace CensusAnalyserTest
     public class Tests
     {
         private string STATE_CENSUS_DATA_PATH = @"C:\Users\de\source\repos\CensusAnalyser\CensusAnalyserTest\resource\StateCensusData.csv";
+
+        private string WRONG_CSV_FILE_PATH =  @"C:\Users\de\source\repos\CensusAnalyser\CensusAnalyserTest\resource\CensusData.csv";
 
         CensusAnalyser.CensusAnalyser censusAnalyser = new CensusAnalyser.CensusAnalyser();
         public void Setup()
@@ -19,5 +22,25 @@ namespace CensusAnalyserTest
             Assert.AreEqual(29, numberOfRecord);
         }
 
+        [Test]
+        public void givenIndianStatesCensusCSVFile_WhenUnMatchNoOfRecord_ThenReturnFalse()
+        {
+            int numberOfRecords = censusAnalyser.loadStateCSVData(STATE_CENSUS_DATA_PATH);
+            Assert.AreNotEqual(30, numberOfRecords);
+        }
+
+        [Test]
+        public void givenIndiaCensusData_WithWrongFile_ShouldThrowCustomException()
+        {
+            try
+            {
+                censusAnalyser.loadStateCSVData(WRONG_CSV_FILE_PATH);
+            }
+            catch (CensusAnalyserException e)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.NO_SUCH_FILE, e.type);
+            }
+
+        }
     }
 }
