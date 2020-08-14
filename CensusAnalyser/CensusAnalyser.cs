09bbbb08b7c3204f114f68685841f96820dbe3ed
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-08-14 20:31:11 +0530

    [Akshay]ADDED:Method To Check Wrong File

--- CensusAnalyser/CensusAnalyser.cs
+++ CensusAnalyser/CensusAnalyser.cs
@@ -7,6 +7,9 @@ namespace CensusAnalyser
     {
         public int loadStateCSVData(string csvFilePath)
         {
+            if (!csvFilePath.Contains("StateCensusData"))
+                throw new CensusAnalyserException("WRONG_FILE_PATH", CensusAnalyserException.ExceptionType.NO_SUCH_FILE);
+
             string[] lines = File.ReadAllLines(csvFilePath);
             return lines.Length - 1;
 
