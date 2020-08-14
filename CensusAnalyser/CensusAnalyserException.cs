e519884938c87f3eaae5b3aa16893d946a00bcf6
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-08-14 20:29:37 +0530

    [Akshay]ADDED:Custom Exception

---
+++ CensusAnalyser/CensusAnalyserException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyser
+{
+    public class CensusAnalyserException : Exception
+    {
+        public enum ExceptionType
+        {
+            NO_SUCH_FILE,
+            NO_SUCH_FILE_TYPE,
+            NO_SUCH_DELIMITER,
+            NO_SUCH_HEADER
+        }
+
+        public ExceptionType type;
+
+        public CensusAnalyserException(String message, ExceptionType type) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
