6135f3e3d1f45fe16ef70fd6fedeb5073e9fd3de
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-08-14 21:04:31 +0530

    [Akshay]ADDED:Method To Check File With Wrong Extension

--- CensusAnalyser/CensusAnalyser.cs
+++ CensusAnalyser/CensusAnalyser.cs
@@ -8,7 +8,15 @@ namespace CensusAnalyser
         public int loadStateCSVData(string csvFilePath)
         {
             if (!csvFilePath.Contains("StateCensusData"))
+            {
                 throw new CensusAnalyserException("WRONG_FILE_PATH", CensusAnalyserException.ExceptionType.NO_SUCH_FILE);
+
+            }
+
+            if (Path.GetExtension(csvFilePath) != ".csv")
+            {
+                throw new CensusAnalyserException("Incorrect file type", CensusAnalyserException.ExceptionType.NO_SUCH_FILE_TYPE);
+            }
 
             string[] lines = File.ReadAllLines(csvFilePath);
             return lines.Length - 1;
